78f860361cd4c734225d0b4833c1d75a795e58b0
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-20 15:42:09 -0500

    added route to corresponding files in server.js

--- server.js
+++ server.js
@@ -11,3 +11,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// route that sends the user to the index page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// route that sends the user to the notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// route that sends the user the db.json file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+});
